164e31a617ac2af8e1a4bfc17e66f29ee2b271d9
Khamidullo <mukhammadjonvoxamidullo@gmail.com>
2024-05-21 15:10:42 +0500

    Some modifications in file order.

--- handlers/start.py
+++ handlers/start.py
@@ -1,45 +1,17 @@
-import logging
-import aiohttp
-import telebot
 import asyncio
-import mysql.connector
-from datetime import datetime
+import aiohttp
+import logging
+import telebot
+from db_users import save_message
+from config import BOT_TOKEN
 
-TELEGRAM_TOKEN = '7061775503:AAEgn5_Q-BBuRxHjIg1F27FUD1QbxGPEhP8'
-ESP32_IP = '192.168.181.217'
-
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'devuser'
-MYSQL_PASSWORD = '4467996m'
-MYSQL_DATABASE = 'telegram_bot_db'
-
-# Connect to MySQL database
-db_connection = mysql.connector.connect(
-    host=MYSQL_HOST,
-    user=MYSQL_USER,
-    password=MYSQL_PASSWORD,
-    database=MYSQL_DATABASE
-)
-db_cursor = db_connection.cursor()
-
-# List of authorized user IDs
-AUTHORIZED_USERS = [1081721793, 1114820537]
-
+AUTHORIZED_USERS = [1081721793, 1114820537, 1270439555]
 logging.basicConfig(level=logging.INFO)
-
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
 def is_authorized(user_id):
     return user_id in AUTHORIZED_USERS
-
-
-def save_message(user_id, username, message_text):
-    timestamp = datetime.now()
-    query = "INSERT INTO user_messages (user_id,username, message_text, message_time) VALUES (%s,%s , %s, %s)"
-    values = (user_id, username, message_text, timestamp)
-    db_cursor.execute(query, values)
-    db_connection.commit()
 
 
 @bot.message_handler(commands=['start'])
@@ -72,5 +44,5 @@ def ring_bell(message):
     save_message(message.from_user.id, message.from_user.username, message.text)
 
 
-if name == "main":
+if __name__ == "__main__":
     bot.polling()
