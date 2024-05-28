0f4588a44d2230569f59f3fcb09a3b64d71f9d27
Khamidullo <mukhammadjonvoxamidullo@gmail.com>
2024-05-28 14:51:54 +0500

    New commands: status and play
    status - receives the latest status update from MQTT broker
    
    play - receives the latest audio update from MQTT broker

--- handlers/start.py
+++ handlers/start.py
@@ -1,14 +1,13 @@
 import asyncio
 import json
-
-import aiohttp
 import logging
+import time
+
 import telebot
 import random
 import paho.mqtt.client as mqtt
 from db_users import save_message
 from config import BOT_TOKEN
-import os
 
 AUTHORIZED_USERS = [1081721793, 1114820537, 1270439555]
 logging.basicConfig(level=logging.INFO)
@@ -22,9 +21,10 @@ PASSWORD = "mqttpass"
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 # Initialize the MQTT client
-mqtt_client = mqtt.Client(protocol=mqtt.MQTTv311)
+mqtt_client = mqtt.Client(client_id=client_id, protocol=mqtt.MQTTv311)
 mqtt_client.username_pw_set(USERNAME, PASSWORD)
 
+latest_status = None
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -32,8 +32,15 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Failed to connect to MQTT broker with result code {rc}", flush=True)
 
+def on_message(client, userdata, msg):
+    global latest_status
+    if msg.topic == MQTT_TOPIC:
+        latest_status = msg.payload.decode()
+        print(f"Received message: {latest_status}", flush=True)
+
 
 mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
 mqtt_client.loop_start()
 
@@ -63,6 +70,48 @@ def ring_bell(message):
 
     bot.reply_to(message, "Bell is ringing!")
     save_message(message.from_user.id, message.from_user.username, message.text)
+
+
+@bot.message_handler(commands=['status'])
+def send_status(message):
+    if not is_authorized(message.from_user.id):
+        bot.reply_to(message, "You are not authorized to use this bot.")
+        return
+
+    global latest_status
+    latest_status = None
+    mqtt_client.publish(MQTT_TOPIC, "status")
+
+    timeout = 10  # seconds
+    start_time = time.time()
+    while latest_status is None and time.time() - start_time < timeout:
+        time.sleep(0.1)
+
+    if latest_status is None:
+        bot.reply_to(message, "Failed to get status update.")
+    else:
+        bot.reply_to(message, latest_status)
+
+
+@bot.message_handler(commands=['play'])
+def send_status(message):
+    if not is_authorized(message.from_user.id):
+        bot.reply_to(message, "You are not authorized to use this bot.")
+        return
+
+    global latest_status
+    latest_status = None
+    mqtt_client.publish(MQTT_TOPIC, "play")
+
+    timeout = 10  # seconds
+    start_time = time.time()
+    while latest_status is None and time.time() - start_time < timeout:
+        time.sleep(0.1)
+
+    if latest_status is None:
+        bot.reply_to(message, "Failed to get status update.")
+    else:
+        bot.reply_to(message, latest_status)
 
 
 @bot.message_handler(content_types=['audio'])
